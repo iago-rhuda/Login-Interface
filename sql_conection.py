c60735044949b2c6d5472ce422bd79dd69d0453b
iago-rhuda <116321371+iago-rhuda@users.noreply.github.com>
2022-10-21 10:15:06 -0300

    Update sql_conection.py

--- sql_conection.py
+++ sql_conection.py
@@ -7,8 +7,8 @@ def create_connection_with_db():
     try:
         connection_data = (
             "Driver={SQL Server};"
-            "Server=EU\TEW_SQLEXPRESS;"
-            "Database=teste_integracao_sql;"
+            "Server=;"
+            "Database=;"
         )
         conection = pyodbc.connect(connection_data)
         cursor = conection.cursor()
@@ -16,11 +16,8 @@ def create_connection_with_db():
         return cursor
     except:
         print(gv.colors['red'] + 'Unsuccessful Connection with database' + gv.colors['end'])
-
 # save data in db
 def save_db(cursor, name, password, birth, city):
-
-    # atentar - se, passar parâmetros com '' para o SQL (não numéricos)
     comand = f'''
     INSERT INTO Users(
 {gv.db_items['user_name']}, {gv.db_items['user_password']}, 
@@ -28,10 +25,7 @@ def save_db(cursor, name, password, birth, city):
     VALUES('{name}', '{password}', '{birth}', '{city}')
     '''
     cursor.execute(comand)
-    # efetivar mudança no bd
     cursor.commit()
-    #print(f"'{name}', '{password}', '{birth}', '{city}'")
-
 # check data in db
 def verify_login(cursor, column, name, password):
     comand = f'''
@@ -54,7 +48,7 @@ def verify_login(cursor, column, name, password):
         print(gv.colors['red'] + 'Access declined' + gv.colors['end'])
         access = 0
         return access
-
+# function to update db data
 def update_db(cursor, item, value, column, id):
     comand = f'''
     UPDATE Users SET {item} = {value}
@@ -62,29 +56,3 @@ def update_db(cursor, item, value, column, id):
     '''
     cursor.execute(comand)
     cursor.commit()
-
-# delete data in db
-def delete_db(cursor, name):
-    comand = f'''
-    DELETE FROM Users
-    '''
-    cursor.execute(comand)
-
-def see_password(cursor, search):
-    # 0 to don't see password
-    # 1 to see password
-    comand = f'''
-    SELECT password_look FROM global_variables
-        WHERE id={search}
-    '''
-    cursor.execute(comand)
-    password = cursor.fetchone()
-    print(password[0])
-    if password[0] == 'False':
-        print('Tá vendo')
-    else:
-        print('Não tá vendo')
-    return password
-
-cursor = create_connection_with_db()
-see_password(cursor, 0)
